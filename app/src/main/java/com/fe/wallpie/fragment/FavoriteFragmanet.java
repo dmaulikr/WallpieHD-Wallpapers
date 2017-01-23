8ec0b7e8ea4ae2c0c88afa918a507068b0b2c2d9
Farmaan-PC <devfarmaan@gmail.com>
2017-01-24 01:25:53 +0530

    Rename FavoriteFragment Layout Name

--- app/src/main/java/com/fe/wallpie/fragment/FavoriteFragmanet.java
+++ app/src/main/java/com/fe/wallpie/fragment/FavoriteFragmanet.java
@@ -29,7 +29,7 @@ public class FavoriteFragmanet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_fragmanet, container, false);
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
